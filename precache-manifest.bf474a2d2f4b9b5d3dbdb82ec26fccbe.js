8b449e4bcbd25cfa3219c351092915bed4b485f9
InterviewCandies <1712162@student.hcmus.edu.vn>
2020-06-05 20:02:55 +0700

    Updates

---
+++ precache-manifest.bf474a2d2f4b9b5d3dbdb82ec26fccbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3636fc23f01fd087e4abd8864cf6d2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25f0c068be4eb5789827",
+    "url": "/static/css/main.a9791eee.chunk.css"
+  },
+  {
+    "revision": "e88e978ec1742b64c854",
+    "url": "/static/js/2.b4b08d71.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/static/js/2.b4b08d71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25f0c068be4eb5789827",
+    "url": "/static/js/main.c73234cc.chunk.js"
+  },
+  {
+    "revision": "b64d6f46e1f7e22a8c5e",
+    "url": "/static/js/runtime-main.b3355253.js"
+  }
+]);
